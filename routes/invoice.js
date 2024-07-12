3473deeb306f0357911fddc16085930399853093
akara <arzukara840@gmail.com>
2024-07-12 13:40:11 +0300

    filter service added

--- routes/invoice.js
+++ routes/invoice.js
@@ -1,23 +1,23 @@
 import express from "express";
 import db from "../db/connection.js";
-import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
+let collection = await db.collection("invoice");
+
 router.get("/", async (req, res) => {
-    let collection = await db.collection("invoice");
     let results = await collection.find({}).toArray();
-    console.log(results);
     res.send(results).status(200);
 });
 
-router.get("/:id", async (req, res) => {
-    let collection = await db.collection("invoice");
-    let query = { _id: new ObjectId(req.params.id) };
-    let result = await collection.findOne(query);
-
-    if (!result) res.send("Not found").status(404);
-    else res.send(result).status(200);
+router.post('/filter', async (req, res) => {
+    const { selectedStatuses } = req.body;
+    try {
+        const invoices = await collection.find({ status: { $in: selectedStatuses } }).toArray();
+        res.send(invoices).status(200);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching invoices' });
+    }
 });
 
 export default router;
